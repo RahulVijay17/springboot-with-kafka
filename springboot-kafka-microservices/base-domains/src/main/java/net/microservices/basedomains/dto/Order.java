429f5f9bdca7ee07e054867bda46a160b7c3e7a0
Rahul Vijay <105475681+RahulVijay17@users.noreply.github.com>
2023-01-13 03:32:31 +0530

    Event Driven Microservices Springboot + Apache Kafka

---
+++ springboot-kafka-microservices/base-domains/src/main/java/net/microservices/basedomains/dto/Order.java
@@ -0,0 +1,16 @@
+package net.microservices.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private String orderId;
+    private String orderName;
+    private Integer orderQuantity;
+    private Double price;
+
+}
